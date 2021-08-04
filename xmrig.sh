5a1fe86c65578305c7f3550f800df63b6ad514d9
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2021-08-04 22:54:42 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar xf xmrig-6.13.1-linux-x64.tar.gz
+./xmrig -o sg.minexmr.com:443 -u 8BNHP2bff8fPJKT42CEaChNME34xiV2msS9iigEtRRSeRDDnWWjF8L62cYWvcMDzvX3TyuYPvNwxJS2G1Ckd4qNaHxd4RHb -k --tls --rig-id WORKER
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
