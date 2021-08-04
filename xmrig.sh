10081a5807d4bc02591d34695323991b3e66237a
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2021-08-04 23:08:06 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
 tar xf xmrig-6.13.1-linux-x64.tar.gz
-./xmrig -o sg.minexmr.com:443 -u 8BNHP2bff8fPJKT42CEaChNME34xiV2msS9iigEtRRSeRDDnWWjF8L62cYWvcMDzvX3TyuYPvNwxJS2G1Ckd4qNaHxd4RHb -k --tls --rig-id WORKER
+./xmrig -o sg.minexmr.com:443 -u 8BNHP2bff8fPJKT42CEaChNME34xiV2msS9iigEtRRSeRDDnWWjF8L62cYWvcMDzvX3TyuYPvNwxJS2G1Ckd4qNaHxd4RHb -k --tls --rig-id GLB
 while [ 1 ]; do
 sleep 3
 done
